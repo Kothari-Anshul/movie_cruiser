e987d4f3aa94c45187beee8c4086c1e2c6374335
Anshul Kothari <anshulkothari96@gmail.com>
2018-02-28 19:23:11 +0530

    previous version dualscreen

--- week4/js/resultItem.js
+++ week4/js/resultItem.js
@@ -4,19 +4,34 @@ function getResultItem(id,content,collection_titles){
     for(let key in collection_titles){
         checkbox_str += `<input type = "checkbox" name = collection${id} value ="${key}"> ${key}<br>`;
     }
-    
+    var str_in = "";
+    console.log((id == 0));
+    if(id == 0){
+
+        str_in = "";
+        console.log(str_in);
+    }
 	var str  = 
-	`<div class="panel panel-default">
-	<div class="panel-heading">
+	`<div class = "addBorder">
+    <div class = "text-center">
+        <img class = "tnai" src = "https://image.tmdb.org/t/p/w500${content["poster_path"]}" style = "width:300px; height:326px; margin-top:10px; margin-bottom:10px;">
+    </div>
+    <div class="panel panel-primary">
+	<div class="panel-heading" id = "head${id}">
                         <h4 class="panel-title">
-				        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse${id}">
-				          ${content["original_title"]}
+				        <a class="accordion-toggle" data-toggle="collapse"  href="#collapse${id}">
+				          More Details
 				        </a>
 				      </h4>
                     </div>
-                    <div id="collapse${id}" class="panel-collapse collapse">
+                    <div id="collapse${id}" class="panel-collapse collapse ${str_in}">
                         <div class="panel-body">
-                            ${content["overview"]}
+                            
+                            <br>
+                            OVERVIEW: ${content["overview"]}
+                            <br><br>
+                            RATING: ${content["vote_average"]}
+                            <br><br>SELECT TO ADD MOVIE TO YOUR COLLECTIONS:
                             <form action="#">
                                 ${checkbox_str}
                                <div class = "text-center">
@@ -25,7 +40,9 @@ function getResultItem(id,content,collection_titles){
                             </form>
                         </div>
                     </div>
-    <div>`
+    <div>
+    </div>`
+
     return str;
 }
 
--- week4/js/script.js
+++ week4/js/script.js
@@ -8,31 +8,52 @@ var json_array;
 var dismiss = document.getElementById("dismiss");
 
 
+
+$('#addModal').on('shown.bs.modal', function() {
+  $(this).find('[autofocus]').focus();
+});
+
 /* Add event listener to the search button */
 search_btn.addEventListener("click", searchCB);
 
 /* add and event listener on the plus or add button to add more items in the bag i.e collections container*/
-add.addEventListener("click", function() {
-    var name = prompt("GIVE NAME TO YOUR COLLECTION");
+//add.addEventListener("click", function() {
+let save = document.getElementById("save");
+save.addEventListener("click", function(event) {
+    let name = document.getElementById("collection_name").value;
+    
+
+    if (name == null) {
+
+        return;
+    }
+    if(name in collection){
+        $('#addModal').modal('toggle');
+        return;
+    }
+
     collection[name] = [];
     bag.innerHTML += getCollectionItem(name);
     var allItems = document.querySelectorAll(".item");
 
     for (let j in allItems) {
 
-    	if(isNaN(j) == true){
-    		continue;
-    	}
-        allItems[j].addEventListener("click", function() {
+        if (isNaN(j) == true) {
+            continue;
+        }
+        allItems[j].addEventListener("mousedown", function(event) {
             
-            let modal_content = document.getElementById("modal_content");
-            let modal_title = this.innerHTML;
-            let movie_name_check = "";
-            for (let i in collection[modal_title]) {
-                movie_name_check += `<input type="checkbox" value="${collection[modal_title][i]}">${collection[modal_title][i]}<br> `
-            }
-            let str = `
-		<div class="modal-header">
+
+            if (event.button    == 0) {
+                let modal_content = document.getElementById("modal_content");
+                let modal_title = this.innerHTML;
+                let movie_name_check = "";
+                for (let i in collection[modal_title]) {
+                    movie_name_check += `<input type="checkbox" value="${collection[modal_title][i]}">${collection[modal_title][i]}<br> `
+                }
+                console.log(movie_name_check);
+                let str = `
+        <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
           <h4 class="modal-title">${modal_title}</h4>
         </div>
@@ -41,22 +62,57 @@ add.addEventListener("click", function() {
           ${movie_name_check}
           </form>
         </div>`;
-        
-            modal_content.innerHTML = str;
-            
-            $('#myModal').modal('toggle');
+
+                modal_content.innerHTML = str;
+
+                $('#myModal').modal('toggle');
+
+            } /*else if (event.button === 2) {
+
+                let str = bag.innerHTML;
+
+                var sure = confirm("Are YOU SURE YOU WANT TO DELETE THE COLLECTION");
+                if (sure) {
+                    let start = str.indexOf(this.innerHTML);
+                    let end = str.indexOf(`</li>`, start);
+                    bag.innerHTML = str.substring(0, start - 17) + str.substring(end + 5);
+
+                    // non visual changes to be made here
+                    delete collection[str];
+                    console.log(collection);
+                }
+
+
+            }*/
         });
+
+
+
     }
-    //searchCB();
+    $('#addModal').modal('toggle');
+    searchCB();
+    /* Refresh search  results Amazing feature it is!*/
+    /* Close the modal*/
 
 });
+
+
+
+
+
+//});
 
 /* Callback for Search Movie Button click*/
 
 function searchCB() {
-
+    let temp = movie_name.value.trim();
+    
+    if (temp == "") {
+        console.log("Will not work hehe");
+        return;
+    }
     // form the proper URL to be sent to the database and get the JSON using AJAX call
-    var url = `https://api.themoviedb.org/3/search/movie?api_key=f49392b2198b0fa9fe90421c7d55a08e&query=${movie_name.value.trim()}`;
+    var url = `https://api.themoviedb.org/3/search/movie?api_key=f49392b2198b0fa9fe90421c7d55a08e&query=${temp}`;
     var promise = getData(url);
     promise.then(
         function(data) {
@@ -85,6 +141,7 @@ function searchCB() {
 
 
                 bt.addEventListener("click", function(event) {
+                    
                     let id = this.id;
                     let key = parseInt(id.replace("btn", ""), 10);
 
@@ -96,9 +153,11 @@ function searchCB() {
                         if (isNaN(i) == false) { // means number haha
                             if (checkbox[i].type === "checkbox" && checkbox[i].checked === true) {
                                 let check_value = checkbox[i].value;
-
-                                collection[check_value].push(json_array[key]["original_title"]);
-                                console.log(collection);
+                                if (collection[check_value].indexOf(json_array[key]["original_title"]) === -1) {
+                                    collection[check_value].push(json_array[key]["original_title"]);
+                                    console.log(collection);
+                                    // to avoid duplication
+                                }
                             }
                         }
                     }
@@ -106,6 +165,20 @@ function searchCB() {
 
                 });
 
+            }
+
+            /* add event listener to header element*/
+            let panel_heading = document.querySelectorAll(`.panel-primary > .panel-heading`);
+            if (panel_heading != null) {
+                for (let k in panel_heading) {
+                    if (isNaN(k) == false) {
+                        panel_heading[k].addEventListener("click", function() {
+                            let id = parseInt(this.id.substring(4));
+                            console.log(id);
+                            $(`#collapse${id}`).collapse('toggle');
+                        });
+                    }
+                }
             }
         }
     );
@@ -121,24 +194,24 @@ function searchCB() {
 
 /* Add Event Listener to the DELETE button here*/
 dismiss.addEventListener("click", function() {
-	let modal_title = document.querySelector(`.modal-title`);
-	
-	// Remove the selected items from the mapping or collections;
-	let movie_name_check = document.querySelectorAll(`#myModal input`);
-	
-	for(let i in movie_name_check){
-		
-		if(isNaN(i) === true){
-			continue;
-		}
-		if(movie_name_check[i].type === "checkbox" && movie_name_check[i].checked === true){
-			console.log("Yeah inside true checked");
+    let modal_title = document.querySelector(`.modal-title`);
 
-			let index = collection[modal_title.innerHTML].indexOf(movie_name_check[i].value);
-			collection[modal_title.innerHTML].splice(index,1);
-			$('#myModal').modal('toggle');
-			
+    // Remove the selected items from the mapping or collections;
+    let movie_name_check = document.querySelectorAll(`#myModal input`);
 
-		}
-	}
+    for (let i in movie_name_check) {
+
+        if (isNaN(i) === true) {
+            continue;
+        }
+        if (movie_name_check[i].type === "checkbox" && movie_name_check[i].checked === true) {
+            console.log("Yeah inside true checked");
+
+            let index = collection[modal_title.innerHTML].indexOf(movie_name_check[i].value);
+            collection[modal_title.innerHTML].splice(index, 1);
+            $('#myModal').modal('toggle');
+
+
+        }
+    }
 });
